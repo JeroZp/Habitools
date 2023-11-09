a26e140bd48df34c735727966da7cd635e2d1620
Jeronimo Perez Baquero <perezjeronimo35@gmail.com>
2023-11-09 10:37:08 -0500

    DataBase

--- Habitools/settings.py
+++ Habitools/settings.py
@@ -81,8 +81,15 @@ WSGI_APPLICATION = 'Habitools.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_habitools',
+        'USER': 'root',
+        'PASSWORD': 'Jero1123',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'sql_mode': 'STRICT_TRANS_TABLES',
+        },
     }
 }
 
