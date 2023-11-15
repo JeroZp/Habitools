1830afd98c1e38434e6ec9530feef1fce0c1a3de
Moisés Arrieta <moisesdavidarh@gmail.com>
2023-11-15 15:57:11 -0500

    nuevo

--- Habitools/settings.py
+++ Habitools/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'db_habitools',
         'USER': 'root',
-        'PASSWORD': 'Jero1123',
+        'PASSWORD': 'Enero1890',
         'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
